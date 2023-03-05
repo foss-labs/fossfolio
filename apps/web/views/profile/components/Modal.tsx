4cc4d0b3d9731e759bcd0f859b677f6c9898e9d2
sreehari2003 <sreeharivijaya2003@gmail.com>
2023-03-05 13:25:51 +0530

    fix: removed env

--- apps/web/views/profile/components/Modal.tsx
+++ apps/web/views/profile/components/Modal.tsx
@@ -1,8 +1,6 @@
 import {
     Button,
     FormControl,
-    FormHelperText,
-    FormLabel,
     Heading,
     Input,
     Modal,
@@ -11,31 +9,38 @@ import {
     ModalContent,
     ModalHeader,
     ModalOverlay,
-    Textarea,
     Flex,
     FormErrorMessage,
+    FormLabel,
+    InputGroup,
+    VStack,
+    InputRightAddon,
 } from '@chakra-ui/react';
 import { InferType } from 'yup';
 import { yupResolver } from '@hookform/resolvers/yup';
-import { SubmitHandler, useForm } from 'react-hook-form';
-import { EventValidator } from '@app/validators';
+import { Controller, SubmitHandler, useFieldArray, useForm } from 'react-hook-form';
+import { regEvent } from '@app/validators';
 
 interface Prop {
     isOpen: boolean;
     onClose: () => void;
 }
 
-type Event = InferType<typeof EventValidator>;
+type Event = InferType<typeof regEvent>;
 
 export const RegisterHack = ({ isOpen, onClose }: Prop) => {
     const {
         register,
+        control,
         handleSubmit,
-        watch,
         formState: { errors },
     } = useForm<Event>({
-        mode: 'onSubmit',
-        resolver: yupResolver(EventValidator),
+        resolver: yupResolver(regEvent),
+    });
+    const { fields, append, remove } = useFieldArray({
+        control,
+        // @ts-ignore
+        name: 'members',
     });
 
     const handleFormData: SubmitHandler<Event> = (data) => {
@@ -54,83 +59,81 @@ export const RegisterHack = ({ isOpen, onClose }: Prop) => {
                     </ModalHeader>
                     <ModalCloseButton />
                     <ModalBody rowGap="30px" display="flex" flexDir="column">
-                        <FormControl isInvalid={!!errors.title}>
-                            <FormLabel fontSize="14px">Name of your Hackathon</FormLabel>
+                        <FormControl isInvalid={!!errors.name}>
+                            <FormLabel fontSize="14px">Enter Team Name</FormLabel>
+                            <Input type="string" placeholder="SlashZero" {...register('name')} />
+                            <FormErrorMessage>Team Name should not be empty</FormErrorMessage>
+                        </FormControl>
+                        <FormControl isInvalid={!!errors.repo}>
+                            <FormLabel fontSize="14px">Repository</FormLabel>
                             <Input
                                 type="string"
-                                placeholder="my hackathon"
-                                {...register('title')}
+                                placeholder="https://github.com/fossfolio/fossfolio"
+                                {...register('repo')}
                             />
-                            <FormErrorMessage>title should not be empty</FormErrorMessage>
+                            <FormErrorMessage>Enter a Valid Repo</FormErrorMessage>
                         </FormControl>
-                        <FormControl isInvalid={!!errors.slug}>
-                            <FormLabel fontSize="14px">Slug</FormLabel>
-                            <Input type="string" placeholder="my hackathon" {...register('slug')} />
-                            <FormHelperText>fossfolio/event/{watch('slug')}</FormHelperText>
-                            <FormErrorMessage>slug should not be empty</FormErrorMessage>
-                        </FormControl>
-                        <FormControl isInvalid={!!errors.eventEnding}>
-                            <FormLabel fontSize="14px">Start Date</FormLabel>
-                            <Input
-                                type="date"
-                                placeholder="my hackathon"
-                                {...register('eventStarting')}
-                            />
-                            <FormErrorMessage>date should not be empty</FormErrorMessage>
-                        </FormControl>
-                        <FormControl isInvalid={!!errors.eventEnding}>
-                            <FormLabel fontSize="14px">End date</FormLabel>
-                            <Input
-                                type="date"
-                                placeholder="my hackathon"
-                                {...register('eventEnding')}
-                            />
-                            <FormErrorMessage>date should not be empty</FormErrorMessage>
-                        </FormControl>
-                        <FormControl isInvalid={!!errors.registrationEnd}>
-                            <FormLabel fontSize="14px">Registartion end Date</FormLabel>
-                            <Input
-                                type="date"
-                                placeholder="my hackathon"
-                                {...register('registrationEnd')}
-                            />
-                            <FormErrorMessage>date should not be empty</FormErrorMessage>
-                        </FormControl>
-                        <FormControl isInvalid={!!errors.description}>
-                            <FormLabel fontSize="14px">Description</FormLabel>
-                            <Textarea
-                                placeholder="Enter a description"
-                                {...register('description')}
-                            />
-                            <FormErrorMessage>description should not be empty</FormErrorMessage>
-                        </FormControl>
-                        <FormControl isInvalid={!!errors.website}>
-                            <FormLabel fontSize="14px">Website</FormLabel>
+                        <FormControl isInvalid={!!errors.lead}>
+                            <FormLabel fontSize="14px">Enter your Email ID</FormLabel>
                             <Input
                                 type="string"
-                                placeholder="mysite.com"
-                                {...register('website')}
+                                placeholder="example@gmail.com"
+                                {...register('lead')}
                             />
-                            <FormErrorMessage>website should not be empty</FormErrorMessage>
+                            <FormErrorMessage>Enter a Valid Email</FormErrorMessage>
                         </FormControl>
-                        <FormControl isInvalid={!!errors.instgram}>
-                            <FormLabel fontSize="14px">Instagram</FormLabel>
-                            <Input
-                                type="string"
-                                placeholder="mysite.com"
-                                {...register('instgram')}
-                            />
-                            <FormErrorMessage>url should not be empmty</FormErrorMessage>
-                        </FormControl>
-                        <FormControl isInvalid={!!errors.twitter}>
-                            <FormLabel fontSize="14px">Twitter</FormLabel>
-                            <Input
-                                type="string"
-                                placeholder="mysite.com"
-                                {...register('twitter')}
-                            />
-                            <FormErrorMessage>url should not be empty</FormErrorMessage>
-                        </FormControl>
+                        <VStack>
+                            <FormControl {...register('members')}>
+                                <FormLabel color="white">Team Members</FormLabel>
+                                {fields.map((member: any, index: number) => (
+                                    <Controller
+                                        name={`members.${index}`}
+                                        key={member.id}
+                                        control={control}
+                                        render={({ field }) => (
+                                            <>
+                                                <FormLabel fontSize="14px">
+                                                    Enter your Team mate Email ID
+                                                </FormLabel>
+                                                <InputGroup
+                                                    key={member[0]}
+                                                    width="100%"
+                                                    marginBottom="20px"
+                                                >
+                                                    <Input
+                                                        ref={field.ref}
+                                                        placeholder="Email ID"
+                                                        onChange={field.onChange}
+                                                        onBlur={field.onBlur}
+                                                        name={`members.${index}.value`}
+                                                        value={field.value as string}
+                                                    />
+                                                    <InputRightAddon
+                                                        onClick={() => remove(index)}
+                                                        _hover={{ cursor: 'pointer' }}
+                                                    >
+                                                        Remove
+                                                    </InputRightAddon>
+                                                </InputGroup>
+                                            </>
+                                        )}
+                                    />
+                                ))}
+                                <FormErrorMessage>Enter a Valid Email</FormErrorMessage>
+                            </FormControl>
+
+                            {fields.length < 3 && (
+                                <Button
+                                    colorScheme="purple"
+                                    fontSize="14px"
+                                    onClick={() => {
+                                        append('');
+                                    }}
+                                >
+                                    ADD Team Member
+                                </Button>
+                            )}
+                        </VStack>
                     </ModalBody>
 
                     <Flex justifyContent="space-around" mt="20px" mb="20px">
