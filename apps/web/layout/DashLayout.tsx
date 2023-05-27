e9f0a123e6ed1b510be373e4f5dded966e0cfb14
Ashmin Jayson <ashminjayson10@gmail.com>
2023-05-28 01:11:35 +0530

    chore: added user auth check to dashboard layout and added .vscode to gitignore

--- apps/web/layout/DashLayout.tsx
+++ apps/web/layout/DashLayout.tsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { fetchUser } from '@app/slices/auth';
 import { Child } from '@app/types';
 import { Flex } from '@chakra-ui/react';
 import { DashNav } from './components/DashNav';
 
-export const DashLayout = ({ children }: Child) => (
-    <Flex>
-        <DashNav />
-        {children}
-    </Flex>
-);
+export const DashLayout = ({ children }: Child) => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+        dispatch<any>(fetchUser());
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    return (
+        <Flex>
+            <DashNav />
+            {children}
+        </Flex>
+    );
+};
