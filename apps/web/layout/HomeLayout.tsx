d15ade6d01a1b874840f6eb7bff82278c744eaeb
Ashmin Jayson <ashminjayson10@gmail.com>
2023-05-28 16:07:52 +0530

    feat: added redirection to home route with provision on login on access to unauthorized routes (#54)
    * chore(routing): removed events from protected routes
    
    * chore(routing): moved array outside useeffect to prevent redeclaration
    
    * chore(routing): moved array outside component to prevent recomputation
    
    * feat: added redirection to home route with provision on login on access to unauthorized route

--- apps/web/layout/HomeLayout.tsx
+++ apps/web/layout/HomeLayout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useRef } from 'react';
 import { Child } from '@app/types';
 import { fetchUser } from '@app/slices/auth';
 import { useRouter } from 'next/router';
@@ -7,16 +7,24 @@ import { authActions } from '@app/slices';
 import { supaClient } from '@app/config/supabaseClient';
 import { MainNav } from './components/MainNav';
 
-const unProtectedRoutes = ['/events'];
+interface NavAuthOptions {
+    redirectLogin: () => void;
+}
+
+const unProtectedRoutes = ['/events', '/404', '/', '/pricing'];
 export const HomeLayout = ({ children }: Child) => {
     const router = useRouter();
     const authState = useSelector((state: any) => state.auth.isLoggedIn);
     const dispatch = useDispatch();
+    const mainNavRef = useRef<NavAuthOptions>(null);
 
     useEffect(() => {
         dispatch<any>(fetchUser());
         if (!unProtectedRoutes.includes(router.pathname)) {
-            if (!authState) router.replace('/');
+            // Push user to home route and send query to open user login modal
+            if (!authState) router.replace('/?authreq=true');
+        } else if (router.query.authreq === 'true') {
+            mainNavRef.current?.redirectLogin();
         }
 
         supaClient.auth.onAuthStateChange((event, session) => {
@@ -24,12 +32,14 @@ export const HomeLayout = ({ children }: Child) => {
                 dispatch(authActions.setLoggedIn({ payload: session?.user.user_metadata }));
             }
         });
+
+        // add router.query to dependencies so invoke useeffect of return to home after unauthorized route access
         // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [authState]);
+    }, [authState, router.query]);
 
     return (
         <>
-            <MainNav />
+            <MainNav ref={mainNavRef} />
             {children}
         </>
     );
