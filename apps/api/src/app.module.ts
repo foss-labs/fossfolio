734426c58e5a3784d0f3bbf3130e06e11e7bcbb1
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-07-13 22:50:52 +0530

    chore: clean api

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -2,8 +2,6 @@ import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { LoggerModule } from 'nestjs-pino';
 import { ConfigModule } from '@nestjs/config';
-import { PrismaModule } from './prisma/prisma.module';
-import { AuthModule } from './auth/auth.module';
 import { AppController } from './app.controller';
 
 @Module({
@@ -12,14 +10,6 @@ import { AppController } from './app.controller';
             isGlobal: true,
             validationSchema: Joi.object({
                 DATABASE_URL: Joi.string(),
-                SUPERTOKENS_APP_NAME: Joi.string(),
-                SUPERTOKENS_API_KEY: Joi.string(),
-                SUPERTOKENS_API_DOMAIN: Joi.string(),
-                SUPERTOKENS_WEBSITE_DOMAIN: Joi.string(),
-                SUPERTOKENS_CONNECTION_URI: Joi.string(),
-                GITHUB_CLIENT_ID: Joi.string(),
-                GITHUB_CLIENT_SECRET: Joi.string(),
-                DASHBOARD_API_KEY: Joi.string(),
             }),
             validationOptions: {
                 allowUnknown: true,
@@ -27,19 +17,6 @@ import { AppController } from './app.controller';
             },
         }),
 
-        PrismaModule,
-        AuthModule.forRoot({
-            connectionURI: process.env.SUPERTOKENS_CONNECTION_URI as string,
-            apiKey: process.env.SUPERTOKENS_API_KEY as string,
-            appInfo: {
-                appName: process.env.SUPERTOKENS_APP_NAME as string,
-                apiDomain: process.env.SUPERTOKENS_API_DOMAIN as string,
-                websiteDomain: process.env.SUPERTOKENS_WEBSITE_DOMAIN as string,
-            },
-            githubClientId: process.env.GITHUB_CLIENT_ID as string,
-            githubClientSecret: process.env.GITHUB_CLIENT_SECRET as string,
-            DashboardApiKey: process.env.DASHBOARD_API_KEY as string,
-        }),
         LoggerModule.forRoot({
             pinoHttp: {
                 level: 'info',
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,19 +1,10 @@
-import supertokens from 'supertokens-node';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { SupertokensExceptionFilter } from './auth/auth.filter';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule, {
         bufferLogs: true,
     });
-    app.enableCors({
-        origin: process.env.SUPERTOKENS_WEBSITE_DOMAIN,
-        allowedHeaders: ['content-type', ...supertokens.getAllCORSHeaders(), 'st-auth-mode'],
-        credentials: true,
-    });
-
-    app.useGlobalFilters(new SupertokensExceptionFilter());
 
     await app.listen(
         (process.env.PORT as string) || 8080,
