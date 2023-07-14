43f9de389e0c9a24b2e656178943d6282c35a9da
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-07-15 00:33:39 +0530

    fix : schema

--- apps/api/prisma/migrations/20230305085813_init/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Tag" ALTER COLUMN "id" SET DEFAULT gen_random_uuid()::TEXT;
--- apps/api/prisma/migrations/20230714185013_remove_scope/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `scope` on the `Account` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "Account" DROP COLUMN "scope";
