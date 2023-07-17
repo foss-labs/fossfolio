65ed315aceb6f8a36d3ba22c0c4ab995ad6733c9
DarkPhoenix2704 <anbarasun123@gmail.com>
2023-07-17 13:41:40 +0530

    fix: env

--- apps/web/config/env.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable turbo/no-undeclared-env-vars */
-export const ENV = {
-    WEBSITE_DOMAIN: process.env.NEXT_PUBLIC_WEBSITE_DOMAIN,
-    API_URL:process.env.NEXT_PUBLIC_SERVER_URL
-};
--- apps/web/config/handler.ts
+++ apps/web/config/handler.ts
@@ -1,8 +1,7 @@
 import Axios from 'axios';
 
 export const apiHandler = Axios.create({
-    // eslint-disable-next-line turbo/no-undeclared-env-vars
-    baseURL: process.env.NEXT_PUBLIC_SERVER_URL,
+    baseURL: process.env.NEXT_PUBLIC_API_URL,
     headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json',
