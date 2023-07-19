89c5789c5a8fd25cedf96120de2b9d77f89cd7d8
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2023-07-20 01:30:06 +0530

    fix: removed unwated try catch block

--- apps/web/hooks/api/Auth/useUser.tsx
+++ apps/web/hooks/api/Auth/useUser.tsx
@@ -2,12 +2,8 @@ import { apiHandler } from '@app/config';
 import { useQuery } from 'react-query';
 
 const getUser = async () => {
-    try {
-        const { data } = await apiHandler.get('/user');
-        return data;
-    } catch (error) {
-        console.log(error);
-    }
+    const { data } = await apiHandler.get('/user');
+    return data;
 };
 
 export const useUser = () => {
