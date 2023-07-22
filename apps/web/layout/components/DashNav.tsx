e1b05cdeb89b5338a9076a8951ff60dab1af0df2
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2023-07-23 00:08:35 +0530

    fix: tailwind and removed uid onn payload

--- apps/web/layout/components/DashNav.tsx
+++ apps/web/layout/components/DashNav.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 import { FiArrowLeft, FiHome, FiUser } from 'react-icons/fi';
 
 export const DashNav = () => (
-    <div className="flex flex-col items-center bg-[#f9f5ff] w-[220px] p-9 pt-[64px] h-[100vh]">
+    <div className="flex flex-col items-center bg-[#f9f5ff] w-[220px] p-9 pt-[64px] h-screen">
         <Image src="/logo.svg" alt="LOGO" width="150" height="150" />
 
         <div className="flex flex-col items-center mt-[20px] gap-[26px] h-[200px]">
