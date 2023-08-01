5c7382300617a64a6f712aa4b2f6ab38dcc7c74e
Reenphy George <reenphygeorge@gmail.com>
2023-08-01 15:16:15 +0530

    feat(api): fixed swagger issue (#75)
    * feat(api): fixed swagger issue
    
    * fix(api): fixed swagger for generic-org dto

--- apps/api/src/organization/dto/create-org.dto.ts
+++ apps/api/src/organization/dto/create-org.dto.ts
@@ -1,10 +1,13 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateOrgDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     name: string;
 
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     slug: string;
--- apps/api/src/organization/dto/generic-org.dto.ts
+++ apps/api/src/organization/dto/generic-org.dto.ts
@@ -1,6 +1,8 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { IsNotEmpty, IsString } from 'class-validator';
 
 export class GenericOrgDto {
+    @ApiProperty()
     @IsString()
     @IsNotEmpty()
     organizationId: string;
