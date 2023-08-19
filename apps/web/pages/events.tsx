b0e8b3c99537bf7c153966fe00e02fdca64be09b
swathipp <84273544+swathipp@users.noreply.github.com>
2023-08-19 21:54:18 +0530

    Screen Loader

--- apps/web/pages/events.tsx
+++ apps/web/pages/events.tsx
@@ -1,20 +1,34 @@
 /* eslint-disable react-hooks/exhaustive-deps */
+import React from 'react';
 import { HomeLayout } from '@app/layout';
 import { EventCard } from '@app/components/events';
 import { NextPageWithLayout } from 'next';
 import { useAllEvents } from '@app/hooks/api/Events';
+import { EventsLoader } from '@app/components/preloaders';
 
-const Events: NextPageWithLayout = () => {
+ const Events: NextPageWithLayout = () => {
     const { isLoading } = useAllEvents();
 
     if (isLoading) {
-        <h1>loading</h1>;
+        return (
+          <div className="p-6 flex flex-col items-center ">
+            <h1 className="text-center text-[48px]">Find Events</h1>
+            <div className="flex flex-wrap flex-col gap-[25px] justify-center p-4  md:justify-start lg:flex-row">
+              <EventsLoader/>
+              <EventsLoader />
+              <EventsLoader />
+              <EventsLoader/>
+              <EventsLoader />
+              <EventsLoader />
+            </div>
+          </div>
+        );
     }
 
     return (
-        <div className="p-6 flex flex-col ">
-            <h1 className="text-center text-[48px]">Find Hackathons</h1>
-            <div className="flex flex-wrap gap-[25px] justify-center lg:justify-start">
+        <div className="p-6 flex flex-col items-center">
+            <h1 className="text-center text-[48px]">Find Events</h1>
+            <div className="flex flex-wrap flex-col gap-[25px] justify-center p-4  md:justify-start lg:flex-row">
                 <EventCard />
                 <EventCard />
                 <EventCard />
@@ -25,5 +39,5 @@ const Events: NextPageWithLayout = () => {
 
 Events.Layout = HomeLayout;
 Events.RequireAuth = false;
+export default Events
 
-export default Events;
