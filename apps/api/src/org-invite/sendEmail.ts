f1d50fc88241b7672eca59288fa90ae8923e5e2b
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2023-09-03 15:31:12 +0530

    fix: react email config

--- apps/api/src/org-invite/sendEmail.ts
+++ apps/api/src/org-invite/sendEmail.ts
@@ -3,19 +3,24 @@ import { InviteUserEmail } from './templates/Orginvite';
 const resend = new Resend('re_123456789');
 
 export type IData = {
-     inviteId:string,
-     from:string,
-     orgName:string
-     fromEmail:string
+    inviteId: string;
+    from: string;
+    orgName: string;
+    fromEmail: string;
 };
 
-export const sendInvite = async (to:string,data:IData): Promise<boolean> => {
+export const sendInvite = async (to: string, data: IData): Promise<boolean> => {
     try {
         await resend.emails.send({
             from: 'Sreehari <sreeharivijaya2003@gmail.com>',
             to: [to],
             subject: 'You have been invited to Join Fossfolio',
-            react: <InviteUserEmail />,
+            react: InviteUserEmail({
+                from: data.from,
+                orgName: data.orgName,
+                inviteId: data.inviteId,
+                fromEmail: data.fromEmail,
+            }),
         });
         return true;
     } catch {
