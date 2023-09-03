95f2b2ed05101240f9a62f2ac2f97d77c72a3f5b
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2023-09-03 16:17:40 +0530

    fix: xcode emulator setup

--- apps/app/App.tsx
+++ apps/app/App.tsx
@@ -5,7 +5,6 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
     </View>
   );
 }
