d82d9f79c62798b099b6357a5905ca6cf7f23c4c
unknown <suryaparvathy2001@gmail.com>
2023-09-04 23:00:15 +0530

    feat: deleteapi

--- apps/web/views/org/components/DeleteModal.tsx
+++ apps/web/views/org/components/DeleteModal.tsx
@@ -1,10 +1,8 @@
+import { useRouter } from 'next/router';
 import { Button } from '@app/ui/components/button';
-import {
-    Dialog,
-    DialogContent,
-    DialogDescription,
-    DialogHeader,
-} from '@app/ui/components/dialog';
+import { Dialog, DialogContent, DialogDescription, DialogHeader } from '@app/ui/components/dialog';
+import { apiHandler } from '@app/config';
+import { toast } from 'sonner';
 
 type IModal = {
     isOpen: boolean;
@@ -12,23 +10,40 @@ type IModal = {
 };
 
 export const DeleteModal = ({ isOpen, onClose }: IModal) => {
+    const router = useRouter();
+    console.log(router);
+    const handleDeleteClick = async () => {
+        try {
+            const { data } = await apiHandler.delete('/org/delete', {
+                data: { organizationId: router.query?.id },
+            });
+            console.log(data);
+            if (!data.ok) {
+                throw new Error();
+            }
+            toast.success('org has been deleted');
+        } catch {
+            toast.error('could not delete org');
+        }
+    };
+
     return (
         <Dialog open={isOpen} onOpenChange={onClose}>
             <DialogContent className="w-[325px] md:w-auto ">
                 <DialogHeader>
                     Delete Org
                     <DialogDescription className="mt-3">
-                        Are you sure? you can&apos;t undo this action afterwards.
+                        Are you sure? you can't undo this action afterwards.
                         <div className="flex justify-end space-x-2 mt-5">
                             <Button
-                                className="text-[black] bg-[#F9F5FF]  border-[1.4px]  hover:text-[white]"
+                                className="text-[black] bg-[#F9F5FF]  border-[1.4px]  hover:text-[white] hover:bg-btn"
                                 onClick={onClose}
                             >
                                 Cancel
                             </Button>
                             <Button
                                 className="text-[white] bg-[red]  border-[1.4px]  "
-                                onClick={() => { }}
+                                onClick={handleDeleteClick}
                             >
                                 Delete
                             </Button>
