8a3fa9d58d30b5fa54c2d5c8d458069240c2cca6
swathipp <84273544+swathipp@users.noreply.github.com>
2023-09-15 18:51:23 +0530

    dashboard participants table

--- apps/web/pages/org/[id]/[event]/index.tsx
+++ apps/web/pages/org/[id]/[event]/index.tsx
@@ -1,13 +1,109 @@
 import { NextPageWithLayout } from 'next';
-import { DashboardLayout } from "@app/layout"
-
+import { DashboardLayout } from "@app/layout";
+import React from 'react';
+import {
+    Table,
+    TableBody,
+    TableCell,
+    TableHead,
+    TableHeader,
+    TableRow,
+} from '@app/ui/components/table';
+import { AiOutlineDelete } from 'react-icons/ai'; 
+import{ FiEdit2 }from 'react-icons/fi';
+export const Members = () => {
+    return (
+        <div className="p-none md:p-[20px]">
+            <Table className="border-[1px] border-[#E9D7FE] rounded-full">
+                <TableHeader className="bg-[#F9FAFB] rounded-lg">
+                    <TableRow>
+                        <TableHead>Name</TableHead>
+                        <TableHead>Student / Pro</TableHead>
+                        <TableHead>Phone</TableHead>
+                        <TableHead>Email address</TableHead>
+                        <TableHead></TableHead>
+                        <TableHead></TableHead>
+                    </TableRow>
+                </TableHeader>
+                <TableBody>
+                    <TableRow>
+                        <TableCell className="font-medium">
+                            <h5>sreehari jayaraj</h5>
+                        </TableCell>
+                        <TableCell>Student</TableCell>
+                        <TableCell>
+                        9745644798
+                        </TableCell>
+                        <TableCell>sreeharivijaya2003@gmail.com</TableCell>
+                        <TableCell className="text-right">
+                            <AiOutlineDelete className="hover:text-[red] cursor-pointer text-lg" />
+                        </TableCell>
+                        <TableCell className="text-right">
+                           <FiEdit2 className="hover:text-[red] cursor-pointer text-lg" />
+                        </TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell className="font-medium">
+                            <h5>sreehari jayaraj</h5>
+                        </TableCell>
+                        <TableCell>Pro</TableCell>
+                        <TableCell>
+                        9745644798  
+                        </TableCell>
+                        <TableCell>sreeharivijaya2003@gmail.com</TableCell>
+                        <TableCell className="text-right">
+                            <AiOutlineDelete className="hover:text-[red] cursor-pointer text-lg" />
+                        </TableCell>
+                        <TableCell className="text-right">
+                           <FiEdit2 className="hover:text-[red] cursor-pointer text-lg" />
+                        </TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell className="font-medium">
+                            <h5>sreehari jayaraj</h5>
+                        </TableCell>
+                        <TableCell>student</TableCell>
+                        <TableCell>
+                        9745644798
+                        </TableCell>
+                        <TableCell>sreeharivijaya2003@gmail.com</TableCell>
+                        <TableCell className="text-right">
+                            <AiOutlineDelete className="hover:text-[red] cursor-pointer text-lg" />
+                        </TableCell>
+                        <TableCell className="text-right">
+                           <FiEdit2 className="hover:text-[red] cursor-pointer text-lg" />
+                        </TableCell>
+                    </TableRow>
+                    <TableRow>
+                        <TableCell className="font-medium">
+                            <h5>sreehari jayaraj</h5>
+                        </TableCell>
+                        <TableCell>Pro</TableCell>
+                        <TableCell>
+                        9745644798
+                        </TableCell>
+                        <TableCell>sreeharivijaya2003@gmail.com</TableCell>
+                        <TableCell className="text-right">
+                            <AiOutlineDelete className="hover:text-[red] cursor-pointer text-lg" />
+                        </TableCell>
+                        <TableCell className="text-right">
+                           <FiEdit2 className="hover:text-[red] cursor-pointer text-lg" />
+                        </TableCell>
+                    </TableRow>
+                </TableBody>
+            </Table>
+        </div>
+    );
+};
 
 const Dashboard: NextPageWithLayout = () => {
-   return (
-      <h1>HELLO DASHBOARD</h1>
-   )
-}
+    return (
+        <div>
+            <Members/>
+        </div>
+    );
+};
 
-Dashboard.Layout = DashboardLayout
-Dashboard.RequireAuth = true
-export default Dashboard
+Dashboard.Layout = DashboardLayout;
+Dashboard.RequireAuth = true;
+export default Dashboard;
