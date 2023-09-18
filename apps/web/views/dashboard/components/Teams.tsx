935651ab365ab3719f14e0743b3cf7ae9bfa704c
unknown <suryaparvathy2001@gmail.com>
2023-09-18 16:32:24 +0530

    feat: teams ui

---
+++ apps/web/views/dashboard/components/Teams.tsx
@@ -0,0 +1,52 @@
+import { Card, CardContent, CardHeader, CardTitle } from '@app/ui/components/card';
+
+export const Teams = () => {
+    return (  
+        <div className="flex justify-between space-x-1 ">
+  <Card className="w-[320px] h-[250px] bg-brand-pink-100 hover:cursor-pointer hover:outline-double hover:outline-[#7F56D9]">
+    <CardHeader className="grid grid-cols-[1fr_110px] items-start gap-4 space-y-0">
+      <div className="space-y-0">
+        <div className="text-xs  ">TEAM</div>
+        <CardTitle className="font-semibold text-sm">Team 6970</CardTitle>
+      </div>
+    </CardHeader>
+    
+    <CardContent>
+      <div className="flex items-center space-x-2">
+        <img src="https://img.freepik.com/free-photo/beautiful-woman-street_23-2147654273.jpg?w=2000" alt="Member 1" className="w-8 h-8 rounded-full" />
+        <div>
+          <div className="font-semibold text-md">Surya</div>
+          <div className="text-xs text-muted-foreground">surya@email.com</div>
+        </div>
+      </div>
+      
+      <div className="flex items-center space-x-2">
+        <img src="https://img.freepik.com/free-photo/beautiful-woman-street_23-2147654273.jpg?w=2000" alt="Member 2" className="w-8 h-8 rounded-full" />
+        <div>
+        <div className="font-semibold text-md">Surya</div>
+          <div className="text-xs text-muted-foreground">surya@email.com</div>
+        </div>
+      </div>
+      
+      <div className="flex items-center space-x-2">
+        <img src="https://img.freepik.com/free-photo/beautiful-woman-street_23-2147654273.jpg?w=2000" alt="Member 3" className="w-8 h-8 rounded-full" />
+        <div>
+        <div className="font-semibold text-md">Surya</div>
+          <div className="text-xs text-muted-foreground">surya@email.com</div>
+        </div>
+      </div>
+      
+      <div className="flex items-center space-x-2 ">
+        <img src="https://img.freepik.com/free-photo/beautiful-woman-street_23-2147654273.jpg?w=2000" alt="Member 4" className="w-8 h-8 rounded-full" />
+        <div>
+        <div className="font-semibold text-md">Surya</div>
+          <div className="text-xs text-muted-foreground">surya@email.com</div>
+        </div>
+      </div>
+    </CardContent>
+  </Card>
+  
+</div>
+
+    );
+}
--- apps/web/views/dashboard/index.ts
+++ apps/web/views/dashboard/index.ts
@@ -1,1 +1,2 @@
 export { Participants } from './components/Participants';
+export { Teams } from './components/Teams';
