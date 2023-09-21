00b8a0a2e54eb4e5b02f16f4b6762882ce44a194
unknown <suryaparvathy2001@gmail.com>
2023-09-21 14:48:27 +0530

    fix:fixed bug in alignment of cards

--- apps/web/pages/org/[id]/teams.tsx
+++ apps/web/pages/org/[id]/teams.tsx
@@ -5,10 +5,11 @@ import { Teams } from '@app/views/dashboard';
 const teams = () => {
     return (
         <div className=" p-6">
-            <h1 className="text-xs">FOSSHACK 3.0</h1>
             <div className="flex justify-between items-center ">
-                <h1 className="text-[38px] font-bold">Registrations</h1>
-
+                <div>
+                    <h1 className="text-xs">FOSSHACK 3.0</h1>
+                    <h1 className="text-[38px] font-bold">Registrations</h1>
+                </div>
                 <div className="flex">
                     <div className="items-center w-[auto] h-[auto] bg-brand-pink-100 rounded-lg p-2 mr-4">
                         <h1 className="text-[30px] text-center font-bold">124</h1>
@@ -29,10 +30,10 @@ const teams = () => {
                 </div>
             </div>
 
-            <div className=" w-[250px] mt-2">
+            <div className=" w-[250px] mt-6 mb-2">
                 <Input type="search" placeholder="Find team or person" />
             </div>
-            <div className="flex flex-wrap flex-col gap-y-[25px] gap-x-[75px] justify-center items-center p-4  md:justify-start lg:flex-row lg:items-center mr-6 ml-6 ">
+            <div className="flex flex-wrap flex-col gap-y-[25px] gap-x-[75px] justify-center items-center p-4  md:justify-start lg:flex-row lg:items-center ">
                 <Teams />
                 <Teams />
                 <Teams />
