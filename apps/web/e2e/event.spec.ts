75381008e2b0c108d19dc6616f876c9165aca0dd
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2023-10-19 19:30:53 +0530

    fix(test): changed locator

--- apps/web/e2e/event.spec.ts
+++ apps/web/e2e/event.spec.ts
@@ -4,7 +4,7 @@ test('should navigate to the events page', async ({ page }) => {
     // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
     await page.goto('/');
     // Find an element with the text 'events' and click on it
-    await page.getByText('events').click();
+    await page.locator('h4').getByText('Events').click();
     // The new url should be "/events" (baseURL is used there)
     await expect(page).toHaveURL('/events');
     // The new page should contain an h1 with "About Page"
--- apps/web/e2e/org.spec.ts
+++ apps/web/e2e/org.spec.ts
@@ -1,8 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('should navigate to org page', async ({ page }) => {
-    const goToOrgBtn = await page.locator('create event');
-    await goToOrgBtn.click();
+    await page.locator('Create Events').click();
 
     await expect(page).toHaveURL('/org');
 });
