7edac9a060401518491047774686476f15fc358b
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-01-14 18:21:14 +0530

    feat: added swagger info for api

--- apps/api/src/org-member/org-member.controller.ts
+++ apps/api/src/org-member/org-member.controller.ts
@@ -4,6 +4,7 @@ import { AuthGuard } from '@nestjs/passport';
 import { RbacGuard } from '../organization/guards/rbac-member.guard';
 import { Roles } from 'src/organization/decorators/roles.decorator';
 import { RemoveMember } from './dto/member-remove.dto';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('org/member')
 export class OrgMemberController {
@@ -16,6 +17,9 @@ export class OrgMemberController {
     }
 
     @Patch('/remove')
+    @ApiTags('org-members')
+    @ApiOperation({ summary: 'Remove members from organization' })
+    @ApiResponse({ status: 200, description: 'User will be removed successfully' })
     @Roles('ADMIN')
     @UseGuards(AuthGuard('jwt'), RbacGuard)
     async removeUser(@Body() data: RemoveMember) {
