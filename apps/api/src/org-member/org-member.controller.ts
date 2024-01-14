5b0e43d142a6fd8f07a52a043fa7375bf2c52848
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-01-14 18:11:58 +0530

    fix(api): added endpoint to remove member by admin

--- apps/api/src/org-member/org-member.controller.ts
+++ apps/api/src/org-member/org-member.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Param, UseGuards } from '@nestjs/common';
+import { Body, Controller, Get, Param, Patch, UseGuards } from '@nestjs/common';
 import { OrganizationMemberService } from './org-member.service';
 import { AuthGuard } from '@nestjs/passport';
 import { RbacGuard } from '../organization/guards/rbac-member.guard';
+import { Roles } from 'src/organization/decorators/roles.decorator';
+import { RemoveMember } from './dto/member-remove.dto';
 
 @Controller('org/member')
 export class OrgMemberController {
@@ -10,6 +12,13 @@ export class OrgMemberController {
     @Get('/:orgID')
     @UseGuards(AuthGuard('jwt'), RbacGuard)
     async getMembers(@Param('orgID') orgID: string) {
-        return this.orgMemberService.getMembers(orgID);
+        return await this.orgMemberService.getMembers(orgID);
+    }
+
+    @Patch('/remove')
+    @Roles('ADMIN')
+    @UseGuards(AuthGuard('jwt'), RbacGuard)
+    async removeUser(@Body() data: RemoveMember) {
+        return await this.orgMemberService.removeMember(data.organisationId, data.memberId);
     }
 }
