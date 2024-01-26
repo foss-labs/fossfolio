53d161efcab1dccb64da420b5778452a9c28407b
NandgopanP <nandgopanp2003@gmail.com>
2024-01-26 16:52:42 +0530

    front page

---
+++ apps/app/myApp/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+import OpenPage from './src/OpenPage';
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <OpenPage/>
+    </View>
+  );
+}
+    
+
+const styles = StyleSheet.create({
+  container: {
+  
+   
+  },
+});
