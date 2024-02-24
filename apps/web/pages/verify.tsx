fc905b56cc15b022873ddd101d13252479286f90
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-02-24 21:40:57 +0530

    fix: verify

--- apps/web/pages/verify.tsx
+++ apps/web/pages/verify.tsx
@@ -9,18 +9,20 @@ type Prop = {
 };
 
 const Verify = ({ orgId }: Prop) => {
-    const router = useRouter();
+
     useEffect(() => {
-        setTimeout(() => {
-            router.push(`/org/${orgId}`);
-            // redirect back to org page after 2.5 seconds
-        }, 2500);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
+        const verify = async () => {
+            const { data } = await apiHandler.get(`/org/invite/verify?id=${orgId}`);
+            console.log(data);
+        };
+        verify();
+    }, [orgId]);
+
+
     return (
         <div className="flex justify-center items-center flex-col h-[90vh] p-7">
             <h1 className="font-extrabold text-transparent text-6xl bg-clip-text bg-gradient-to-r from-purple-400 to-pink-600">
-                EMAIL VERIFICATION SUCCESFULL
+                Verifying your email
             </h1>
         </div>
     );
@@ -28,37 +30,10 @@ const Verify = ({ orgId }: Prop) => {
 
 // used to verify the email for org
 export async function getServerSideProps(ctx: any) {
-    const accesToken = ctx.req.cookies['access_token'];
-    const refreshToken = ctx.req.cookies['refresh_token'];
-
-    if (!accesToken) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        };
-    }
-
-    const { data } = await apiHandler.get(`/org/invite/verify?id=${ctx.query.id}`, {
-        headers: {
-            Cookie: `access_token=${accesToken}; refresh_token=${refreshToken};`,
-        },
-    });
-
-    if (!data.ok) {
-        return {
-            redirect: {
-                destination: '/',
-                permanent: false,
-            },
-        };
-    }
-
     return {
         props: {
             ok: true,
-            orgId: data.data.id,
+            orgId: ctx.query.id,
         },
     };
 }
