8d53b05a89c3ec8f35bbd8d89b1d1f98bc9b0323
DarkPhoenix2704 <anbarasun123@gmail.com>
2024-02-26 13:25:28 +0530

    fix: prod

--- apps/web/pages/api/generate.ts
+++ apps/web/pages/api/generate.ts
@@ -2,11 +2,22 @@ import OpenAI from 'openai';
 import type { NextApiRequest, NextApiResponse } from 'next/types';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+    console.log(req.body)
+    
     if (req.method === 'POST') {
         const gpt = new OpenAI({
             apiKey: process.env.OPEN_AI_TOKEN,
         });
-        const { prompt } = JSON.parse(req.body);
+        const prompt = JSON.parse(req.body);
+
+        if(!prompt?.prompt) {
+            res.status(400).json({
+                ok: false,
+                message: 'Prompt is required',
+            });
+            return;
+        }
+
         try {
             const completion = await gpt.chat.completions.create({
                 messages: [
@@ -16,15 +27,12 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
                             'You are an AI writing assistant that continues existing text based on context from prior text. ' +
                             'Give more weight/priority to the later characters than the beginning ones. Make sure to construct complete sentences.',
                     },
-                    { role: 'user', content: prompt },
+                    { role: 'user', content: prompt?.prompt },
                 ],
                 model: 'gpt-3.5-turbo',
             });
 
             res.status(200).send(completion.choices[0].message.content as string);
-            // res.status(200).send(
-            //     'Hello this is how ei generations works , when ever user eneter ++ in theier ui open ai api endspoints are called , pretty good right?',
-            // );
         } catch {
             res.status(500).json({
                 ok: false,
