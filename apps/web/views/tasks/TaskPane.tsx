4e03fcc72ffbb8df9e6a0403908dea5ab776085c
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-03-29 02:12:15 +0530

    feat: novel editor inside kanban pane

--- apps/web/views/tasks/TaskPane.tsx
+++ apps/web/views/tasks/TaskPane.tsx
@@ -1,3 +1,13 @@
+import { Button } from '@app/components/ui/Button';
+import { Input } from '@app/ui/components/input';
+import {
+    Select,
+    SelectContent,
+    SelectItem,
+    SelectTrigger,
+    SelectValue,
+} from '@app/ui/components/select';
+import { Editor } from 'novel';
 import Drawer from 'react-modern-drawer';
 import 'react-modern-drawer/dist/index.css';
 
@@ -6,16 +16,52 @@ type Props = {
     onClose: () => void;
 };
 
+const InputOption = [
+    {
+        label: 'sreehari',
+        value: '1234',
+    },
+    {
+        label: 'gopan',
+        value: 'erre',
+    },
+];
+
 export const TaskPane = ({ open, onClose }: Props) => {
     return (
         <Drawer
             open={open}
             onClose={onClose}
             direction="right"
-            size={850}
-            className="p-5 bg-red-300"
+            size={650}
+            className="p-5 bg-red-300 overflow-y-scroll"
         >
-            <h1>Hello world</h1>
+            <div className="flex flex-col">
+                <Input placeholder="title" />
+                <article className="mt-3">
+                    <Select>
+                        <SelectTrigger id="type">
+                            <SelectValue placeholder="Assign users" />
+                        </SelectTrigger>
+
+                        <SelectContent position="popper">
+                            {InputOption.map((el) => (
+                                <SelectItem key={el.value as string} value={el.value as string}>
+                                    {el.label}
+                                </SelectItem>
+                            ))}
+                        </SelectContent>
+                    </Select>
+                </article>
+                <article className="border-2 rounded-sm mt-3 min-h-[700px]">
+                    <Editor
+                        className="w-full"
+                        completionApi={`/api/generate`}
+                        storageKey="kanban-editor"
+                    />
+                </article>
+                <Button className="mt-3">Save</Button>
+            </div>
         </Drawer>
     );
 };
