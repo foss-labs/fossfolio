0fa749da90df7876bf4aef7c4148d0c9ada6d5a0
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-03-30 21:06:32 +0530

    fix(web): ts version mismatch

--- apps/web/env.d.ts
+++ apps/web/env.d.ts
@@ -3,7 +3,5 @@ declare namespace NodeJS {
         NEXT_PUBLIC_WEBSITE_DOMAIN: string;
         NEXT_PUBLIC_API_URL: string;
         OPEN_AI_TOKEN: string;
-        AWS_ACCESS_KEY: string;
-        AWS_SECRET_KEY: string;
     }
 }
