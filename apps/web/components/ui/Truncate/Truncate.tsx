b7652a77c24d6d44b6da93c24e3dd99d2007ac0e
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-04-14 17:13:49 +0530

    feat: added tooltip and truncate

---
+++ apps/web/components/ui/Truncate/Truncate.tsx
@@ -0,0 +1,30 @@
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipProvider,
+    TooltipTrigger,
+} from '@app/ui/components/tooltip';
+
+interface Prop {
+    text: string;
+    size?: number;
+}
+
+export const Truncate = ({ text, size = 10 }: Prop) => {
+    const textSize = text.length;
+
+    if (textSize > size) {
+        return (
+            <TooltipProvider>
+                <Tooltip>
+                    <TooltipTrigger>{text}</TooltipTrigger>
+                    <TooltipContent>
+                        <p>{text.slice(0, 9)}....</p>
+                    </TooltipContent>
+                </Tooltip>
+            </TooltipProvider>
+        );
+    } else {
+        return <p>{text}</p>;
+    }
+};
