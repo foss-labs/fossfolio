6ccd13acebc4ceaa1fd654940291f4a9f0a3f4fe
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-05-10 18:59:33 +0530

    fix: added event service

--- apps/api/src/form/form.module.ts
+++ apps/api/src/form/form.module.ts
@@ -1,13 +1,14 @@
 import { Module } from '@nestjs/common';
 import { PrismaModule } from '../prisma/prisma.module';
-import { CloudModule } from '../cloud/cloud.module';
 import { AiModule } from '../ai/ai.module';
 import { FormController } from './form.controller';
 import { FormService } from './form.service';
+import { EventsService } from '../events/events.service';
+import { EventsModule } from '../events/events.module';
 
 @Module({
-    providers: [FormService],
-    imports: [PrismaModule, CloudModule, AiModule],
+    providers: [FormService, EventsService],
+    imports: [PrismaModule, AiModule, EventsModule],
     controllers: [FormController],
 })
 export class FromModule {}
