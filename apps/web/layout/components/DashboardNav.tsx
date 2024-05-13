7af0d871528abf71b41a7dee416e2fa30977e7ca
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-05-13 15:05:45 +0530

    feat: tabs animation

--- apps/web/layout/components/DashboardNav.tsx
+++ apps/web/layout/components/DashboardNav.tsx
@@ -6,12 +6,20 @@ import { FiArrowLeft } from 'react-icons/fi';
 import { apiHandler } from '@app/config';
 import { toast } from 'sonner';
 import { useRoles, useToggle } from '@app/hooks';
+import { motion } from 'framer-motion';
 import { Button } from '@app/components/ui/Button';
+import { useMemo } from 'react';
 
 export const DashNav = () => {
     const router = useRouter();
 
     const { canEditEvent } = useRoles();
+
+    const page = useMemo(() => {
+        return router.pathname.split('/')[4];
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [router]);
 
     const { id, pk } = router.query;
 
@@ -63,26 +71,42 @@ export const DashNav = () => {
         <div className="flex fixed w-full p-3 justify-between bg-brand-pink-100 ">
             <div className="flex">
                 <div
-                    className="font-medium flex text-lg pr-3 hover:cursor-pointer  justify-center items-center gap-3"
+                    className="font-small flex text-lg pr-3 hover:cursor-pointer justify-center items-center gap-3"
                     onClick={() => router.push(`/org/${id}`)}
                 >
                     <FiArrowLeft className="mr-2.1" />
-                    Go Back
                 </div>
                 <Tabs defaultValue="Event" className="w-[400px]">
                     <TabsList>
-                        {ROUTES.map(({ name, icon }) => {
+                        {ROUTES.map(({ name }) => {
                             if ((name === 'Revenue' && !isPaidEvent) || name === 'Tasks') return;
                             return (
-                                <TabsTrigger
+                                <button
+                                    key={name}
                                     onClick={() => {
                                         router.push(`/org/${id}/${pk}/${name.toLowerCase()}`);
                                     }}
-                                    value={name}
+                                    className={`${
+                                        page === name.toLowerCase() ? '' : 'hover:text-black/60'
+                                    } relative rounded-full px-3 py-1.5 text-sm font-medium text-black outline-sky-400 transition focus-visible:outline-2`}
+                                    style={{
+                                        WebkitTapHighlightColor: 'transparent',
+                                    }}
                                 >
-                                    {' '}
-                                    {name}{' '}
-                                </TabsTrigger>
+                                    {page === name.toLowerCase() && (
+                                        <motion.span
+                                            layoutId="bubble"
+                                            className="absolute inset-0 z-10 bg-white mix-blend-difference text-black"
+                                            style={{ borderRadius: 10 }}
+                                            transition={{
+                                                type: 'spring',
+                                                bounce: 0.2,
+                                                duration: 0.6,
+                                            }}
+                                        />
+                                    )}
+                                    {name}
+                                </button>
                             );
                         })}
                     </TabsList>
