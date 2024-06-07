798b4569c178155b1cc13453aaa0177157cbc203
sreehari jayaraj <sreeharivijaya2003@gmail.com>
2024-06-07 20:40:57 +0530

    fix: org service"

--- apps/api/src/models/Org.ts
+++ apps/api/src/models/Org.ts
@@ -1,13 +1,38 @@
-import BaseModel from '@api/models/BaseModel';
-import { Organization } from '@api/db/schema';
-import { Logger } from '@nestjs/common';
-import { SystemTable } from '@api/utils/db';
+import BaseModel from "@api/models/BaseModel";
+import { Organization } from "@api/db/schema";
+import { Logger } from "@nestjs/common";
+import { SystemTable } from "@api/utils/db";
+import BaseContext from "@api/BaseContext";
+import { FFError } from "@api/utils/error";
+import { Knex } from "knex";
 
 export class OrgModel extends BaseModel<SystemTable.Org, Organization>(
-	SystemTable.Org,
+  SystemTable.Org
 ) {
-	constructor() {
-		const logger = new Logger('Org Model');
-		super(logger);
-	}
+  constructor() {
+    const logger = new Logger("Org Model");
+    super(logger);
+  }
+
+  static async getOrgWithMemberInfo(orgId: string, trx?: Knex) {
+    try {
+      const queryBuilder = trx ?? BaseContext.knex;
+      const orgWithMember = await queryBuilder
+        .select(`${SystemTable.Org}.*`, `${SystemTable.OrgMember}.*`)
+        .from(SystemTable.Org)
+        .leftJoin(
+          SystemTable.OrgMember,
+          `${SystemTable.Org}.id`,
+          `${SystemTable.OrgMember}.fk_organization_id`
+        )
+        .where({
+          [`${SystemTable.Org}.id`]: orgId,
+        });
+
+      return orgWithMember;
+    } catch (error) {
+      FFError.databaseError(`${SystemTable.Org}: Query Failed: `, error);
+      throw error; // Ensure the error is rethrown
+    }
+  }
 }
